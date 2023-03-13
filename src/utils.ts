9fd503c90696daf68a2982d217d1f64873c599e4
djmbritt <david@effect.ai>
2023-03-13 14:55:45 +0100

    Update utilities

---
+++ src/utils.ts
@@ -0,0 +1,33 @@
+import { Campaign } from '@effectai/effect-js/dist/lib/types/campaign.js'
+import { efxTaskProxy } from './effect.js'
+import { env } from './env.js';
+
+export const getAccountBalance = async () => {
+    const efxRow = (await efxTaskProxy.api.rpc.get_currency_balance(
+        String(efxTaskProxy?.config?.efxTokenContract), 
+        env.EFXTASKPROXY_ACCOUNT_NAME, 
+        efxTaskProxy.config.efxSymbol))[0]
+    if (efxRow) {
+        return parseFloat(efxRow.replace(` ${efxTaskProxy.config.efxSymbol}`, ''))
+    } else {
+        return 0
+    }
+}
+
+export const printPrice = async (campaignId: number, reps: number, tasks: number) => {
+    try {
+        const campaign: Campaign = await efxTaskProxy.force.getCampaign(campaignId, true)
+        const costs = Number(campaign?.info?.reward) * reps * tasks
+        const balance = await getAccountBalance()
+        console.log(
+            `Creating new tasks for:\n`, 
+            `#${campaignId}. ${campaign?.info?.title}\n`,
+            `${tasks} tasks of ${reps} reps at ${campaign?.info?.reward} EFX/task\n`,
+            `Total: ${costs} EFX\n`,
+            `Balance: ${balance} EFX\n`,
+        )
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
